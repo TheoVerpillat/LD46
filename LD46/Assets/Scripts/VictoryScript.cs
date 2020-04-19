85d8ae3c300e4d4696ed37fdcef7b577e76a5e42
Azathothep <32431387+Azathothep@users.noreply.github.com>
2020-04-19 18:42:45 +0200

    [ADD] Victory Canvas

---
+++ LD46/Assets/Scripts/VictoryScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VictoryScript : MonoBehaviour
+{
+    public bool victory;
+
+    [SerializeField] GameObject victoryCanvas;
+    [SerializeField] Animator anim;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (victory)
+        {
+            victoryCanvas.SetActive(true);
+            anim.SetTrigger("Appear");
+
+            victory = false;
+        }
+    }
+}
