c928ac900469609d10863d6a53bf37e86dad05c2
Azathothep <32431387+Azathothep@users.noreply.github.com>
2020-04-20 16:48:04 +0200

    Item prefabs + How to Play screens

--- LD46/Assets/Scripts/Menu/MenuScript.cs
+++ LD46/Assets/Scripts/Menu/MenuScript.cs
@@ -16,6 +16,11 @@ public class MenuScript : MonoBehaviour
     [SerializeField] private GameObject page1;
     [SerializeField] private GameObject page2;
     [SerializeField] private GameObject page3;
+    [SerializeField] private GameObject page4;
+    [SerializeField] private GameObject page5;
+    [SerializeField] private GameObject page6;
+    [SerializeField] private GameObject page7;
+
     private int currentPage;
 
     private GameObject backCanvas;
@@ -110,17 +115,43 @@ public class MenuScript : MonoBehaviour
             currentPage = 2;
             page2.SetActive(true);
             page1.SetActive(false);
-        } else if (currentPage == 2)
+        }
+        else if (currentPage == 2)
         {
             currentPage = 3;
             page3.SetActive(true);
             page2.SetActive(false);
-        } else if (currentPage == 3)
+        }
+        else if (currentPage == 3)
+        {
+            currentPage = 4;
+            page4.SetActive(true);
+            page3.SetActive(false);
+        }
+        else if (currentPage == 4)
+        {
+            currentPage = 5;
+            page5.SetActive(true);
+            page4.SetActive(false);
+        }
+        else if (currentPage == 5)
+        {
+            currentPage = 6;
+            page6.SetActive(true);
+            page5.SetActive(false);
+        }
+        else if (currentPage == 6)
+        {
+            currentPage = 7;
+            page7.SetActive(true);
+            page6.SetActive(false);
+        }
+        else if (currentPage == 7)
         {
             currentPage = 1;
             page1.SetActive(true);
             backCanvas.SetActive(true);
-            page3.SetActive(false);
+            page7.SetActive(false);
             howToCanvas.SetActive(false);
         }
     }
@@ -142,6 +173,26 @@ public class MenuScript : MonoBehaviour
             currentPage = 2;
             page3.SetActive(false);
             page2.SetActive(true);
+        } else if (currentPage == 4)
+        {
+            currentPage = 3;
+            page4.SetActive(false);
+            page3.SetActive(true);
+        } else if (currentPage == 5)
+        {
+            currentPage = 4;
+            page5.SetActive(false);
+            page4.SetActive(true);
+        } else if (currentPage == 6)
+        {
+            currentPage = 5;
+            page6.SetActive(false);
+            page5.SetActive(true);
+        } else if (currentPage == 7)
+        {
+            currentPage = 6;
+            page7.SetActive(false);
+            page6.SetActive(true);
         }
     }
 }
