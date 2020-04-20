ec7fae24443d541110a14bbac30de9330ecb4386
Azathothep <32431387+Azathothep@users.noreply.github.com>
2020-04-20 12:31:24 +0200

    UIPause graphics

--- LD46/Assets/Scripts/Menu/PauseScript.cs
+++ LD46/Assets/Scripts/Menu/PauseScript.cs
@@ -6,17 +6,24 @@ public class PauseScript : MonoBehaviour
 {
     private bool isPaused = false;
     [SerializeField] private GameObject pauseCanvas;
+    [SerializeField] private AudioSource buttonSound;
+    private bool esc = false;
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
+            esc = true;
             Pause();
         }
     }
 
     public void Pause()
     {
+        if (!esc) {
+            if (buttonSound) { buttonSound.Play(); }
+        }
+
         if (isPaused)
         {
             pauseCanvas.SetActive(false);
@@ -27,6 +34,8 @@ public class PauseScript : MonoBehaviour
             pauseCanvas.SetActive(true);
             isPaused = true;
         }
+
+        esc = false;
     }
 }
 
